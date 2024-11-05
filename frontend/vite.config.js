52794fe5aa5b878d7b81d14c124f140e2d62c276
md nurullah <nurullah7733@gmail.com>
2024-11-05 14:55:36 +0600

    1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "build", // Ensures the output directory is named "dist"
-  },
+
   server: {
     port: 3000,
     proxy: {
